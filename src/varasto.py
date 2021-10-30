71988efdab22002f38173b2a2d992059bbca165a
shlevanto <simo.levanto@helsinki.fi>
2021-10-31 00:00:33 +0300

    Korjattiin testin hajoittava osuus.

--- src/varasto.py
+++ src/varasto.py
@@ -2,11 +2,11 @@ class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
-        #github actions -> hajoita koodi
-        #else:
+
+        else:
             # virheellinen, nollataan
-        #    self.tilavuus = 0.0
-        
+            self.tilavuus = 0.0
+
         if alku_saldo < 0.0:
             # virheellinen, nollataan
             self.saldo = 0.0
